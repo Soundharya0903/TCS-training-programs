29056507b032d4afd4e514d234b325d4af319b40
Soundharya Gomathi S <114283701+Soundharya0903@users.noreply.github.com>
2023-07-26 11:28:41 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int fact(int n);
+int main()
+{
+    int n,i,m,rem=0;
+    scanf("%d",&n);
+    m=fact(n);
+    rem=m%10;
+    m=m/10;
+    if(rem==0)
+    {
+        printf("%d",m%10);
+    }
+    else{
+        printf("%d",rem);
+    }
+    
+}
+int fact(int n)
+{
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
